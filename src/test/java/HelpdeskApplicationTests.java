c4d0f381c8bf1d7e4674a6fce5fb4f652c9899a1
lamiaesaib <saiblamiae@gmail.com>
2026-06-30 20:13:38 +0100

    fix: move tests to src/test

--- src/test/java/HelpdeskApplicationTests.java
+++ src/test/java/HelpdeskApplicationTests.java
@@ -2,14 +2,12 @@ package com.devops.helpdesk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class HelpdeskApplicationTests {
 
     @Test
     void contextLoads() {
-        
     }
+
 }
